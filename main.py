4f8bd788de4b501d5daf0e039f427a34da8dfa52
Salawat9048 <salawat9048@outlook.com>
2024-06-09 11:23:14 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,18 @@
+my_dict ={'salawat': 1991, 'musin': 1992, 'ildar': 1963}
+print(my_dict)
+print(my_dict['salawat'])
+print(my_dict.get('family'))
+my_dict.update({'sister': 2001, 'brother': 2003})
+print(my_dict)
+my_dict['mom'] = 1964
+print(my_dict)
+a = my_dict.pop('mom')
+print(my_dict)
+print(a)
+my_set ={1, 1, 2, 2, 3, 3, 4, 4}
+print(my_set)
+my_set.add(5)
+my_set.add(6)
+print(my_set)
+my_set.remove(1)
+print(my_set)
